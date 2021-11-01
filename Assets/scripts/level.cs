11dcc1529c0a57317983f6a7de2c840f1ea899ff
George Lamb <s211427@students.aie.edu.au>
2021-11-01 16:03:03 +1100

    add level generation

---
+++ Assets/scripts/level.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level : MonoBehaviour {
+	public GameObject pipe;
+	public player p;
+
+	List<GameObject> pipes = new List<GameObject>();
+
+	void Start() {
+
+	}
+
+	public void on_new_pipe_needed() {
+		float hole_pos = Random.Range(-3, 3);
+
+		var ps = pipe.GetComponent<Transform>().localScale;
+
+		/* Upper pipe */
+		var new_pipe = Instantiate(pipe, new Vector3(p.transform.position.x + 10, ((ps.y / 2.0f) + 2.0f) + hole_pos, 0), Quaternion.identity);
+
+		pipes.Add(new_pipe);
+
+		/* Lower pipe */
+		new_pipe = Instantiate(pipe, new Vector3(p.transform.position.x + 10, -((ps.y / 2.0f) + 2.0f) + hole_pos, 0), Quaternion.identity);
+
+		pipes.Add(new_pipe);
+
+		List<GameObject> to_remove = new List<GameObject>();
+		foreach (var c in pipes) {
+			if (c.transform.position.x < p.transform.position.x - 10) {
+				to_remove.Add(c);
+			}
+		}
+
+		foreach (var c in to_remove) {
+			Destroy(c);
+			pipes.Remove(c);
+		}
+	}
+}
--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 public class player : MonoBehaviour {
 	Rigidbody rb;
 
-	public float speed = 500;
+	public float speed = 350;
 	public float jump_force = 300;
+	public level l;
+
+	float timer = 0.0f;
 
 	void Start() {
 		rb = GetComponent<Rigidbody>();
@@ -19,6 +22,14 @@ public class player : MonoBehaviour {
 		if (Input.GetKeyDown("space")) {
 			rb.velocity = new Vector3(rb.velocity.x, 0, 0);
 			rb.AddForce(new Vector3(0, jump_force, 0));
+		}
+
+
+		/* The timer makes sure the pipes aren't spawned continously. */
+		timer += Time.deltaTime;
+		if (transform.position.x > 10 && (int)transform.position.x % 10 == 0 && timer > 0.3f) {
+			l.on_new_pipe_needed();
+			timer = 0.0f;
 		}
 	}
 }
